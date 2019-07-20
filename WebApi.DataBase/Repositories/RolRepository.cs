6c7430a9307b9dc7be64ac06f44cd68edb16ae66
Ivan C <ivandres73@unitec.edu>
2019-07-20 17:57:12 -0600

    halfway there

---
+++ WebApi.DataBase/Repositories/RolRepository.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="RolRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace WebApi.DataBase.Repositories
+{
+    using System;
+    using System.Linq;
+    using WebApi.DataBase.Context;
+    using WebApi.DataBase.Models;
+
+    public class RolRepository : WebApiRepositoryBase<Rol>
+    {
+        public RolRepository(WebApiContext context)
+           : base(context)
+        {
+        }
+
+        public override IQueryable<Rol> All()
+        {
+            return this.Context.Rol;
+        }
+
+        protected override Rol MapNewValuesToOld(Rol oldEntity, Rol newEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
